fcf0514a160a63c8a239726f667224b5ab50df59
Carl Quivron <carl.quivron@gmail.com>
2018-03-06 10:56:45 +0100

    delete unused Typography component

--- frontend/src/components/evaluation/FormEvaluation.js
+++ frontend/src/components/evaluation/FormEvaluation.js
@@ -1,6 +1,6 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import {TextField, Button, Typography, withStyles} from 'material-ui';
+import {TextField, Button, withStyles} from 'material-ui';
 
 const styles = {};
 
