930bf14f3d4251369a2da7e259711fb89117e73b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-07 13:41:40 +0000

    Deploy EBM-2017-2018/MarkUs to github.com/EBM-2017-2018/MarkUs.git:gh-pages

--- api_data.js
+++ api_data.js
@@ -1,6 +1,31 @@
 define({ "api": [
   {
     "type": "get",
+    "url": "/evaluations",
+    "title": "Récupère la liste de toutes les évaluations",
+    "name": "GetEvaluations",
+    "group": "Evaluations",
+    "description": "<p>Cette URL renvoit un JSON contenant toutes les évaluations créées.</p> <p>Les informations requises actuellement est juste le champs &quot;name&quot; de l'évaluations.</p>",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "   H[\n{\n    \"date\": \"2018-03-07T13:31:24.528Z\",\n    \"questions\": [],\n    \"_id\": \"5a9fe9ac7dce47135f250cac\",\n    \"name\": \"Evaluation 1\",\n    \"__v\": 0\n},\n{\n    \"date\": \"2018-03-07T13:31:29.728Z\",\n    \"questions\": [],\n    \"_id\": \"5a9fe9b17dce47135f250cad\",\n    \"name\": \"Evaluation 2\",\n    \"__v\": 0\n}\n]",
+          "type": "html"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/evaluations/index.js",
+    "groupTitle": "Evaluations",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/evaluations"
+      }
+    ]
+  },
+  {
+    "type": "get",
     "url": "/",
     "title": "Hello World",
     "name": "GetHome",
