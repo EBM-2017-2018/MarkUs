f4a4f637eb2ef7d519fd2b57fdfbd8e1528d3df1
Cissé KANE <cisskane@gmail.com>
2018-03-19 11:35:55 +0100

    message pour update

--- backend/src/api/evaluations/evaluationController.js
+++ backend/src/api/evaluations/evaluationController.js
@@ -82,7 +82,7 @@ module.exports.update = (req, res) => {
         if (err) {
           return res.json(err);
         }
-        return res.end();
+        return res.json({ update: 'done' });
       },
     );
   }
