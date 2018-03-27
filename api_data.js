eec88b305c68d908dbefe1fa57152165a95ed45d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-27 06:36:55 +0000

    Deploy EBM-2017-2018/MarkUs to github.com/EBM-2017-2018/MarkUs.git:gh-pages

--- api_data.js
+++ api_data.js
@@ -245,11 +245,47 @@ define({ "api": [
     ]
   },
   {
+    "type": "delete",
+    "url": "/feedbacks/:id",
+    "title": "Supprime un feedback identifié par son Id",
+    "name": "DeleteFeedbackById",
+    "group": "Feedbacks",
+    "description": "<p>Supprime le feedback identifié par son Id</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID du feedback</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "id: 5aa00cbddfc165256122dccc,",
+          "type": "string"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/feedbacks/index.js",
+    "groupTitle": "Feedbacks",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/feedbacks/:id"
+      }
+    ]
+  },
+  {
     "type": "post",
     "url": "/feedbacks",
     "title": "Crée un feedback",
     "name": "DeleteFeedbackById",
-    "group": "Feedback",
+    "group": "Feedbacks",
     "description": "<p>Crée un feedback</p>",
     "parameter": {
       "fields": {
@@ -294,46 +330,10 @@ define({ "api": [
     },
     "version": "0.0.0",
     "filename": "src/api/feedbacks/index.js",
-    "groupTitle": "Feedback",
+    "groupTitle": "Feedbacks",
     "sampleRequest": [
       {
         "url": "http://localhost:4000/api/feedbacks"
-      }
-    ]
-  },
-  {
-    "type": "delete",
-    "url": "/feedbacks/:id",
-    "title": "Supprime un feedback identifié par son Id",
-    "name": "DeleteFeedbackById",
-    "group": "Feedback",
-    "description": "<p>Supprime le feedback identifié par son Id</p>",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "String",
-            "optional": false,
-            "field": "id",
-            "description": "<p>ID du feedback</p>"
-          }
-        ]
-      },
-      "examples": [
-        {
-          "title": "Request-Example:",
-          "content": "id: 5aa00cbddfc165256122dccc,",
-          "type": "string"
-        }
-      ]
-    },
-    "version": "0.0.0",
-    "filename": "src/api/feedbacks/index.js",
-    "groupTitle": "Feedback",
-    "sampleRequest": [
-      {
-        "url": "http://localhost:4000/api/feedbacks/:id"
       }
     ]
   },
@@ -342,7 +342,7 @@ define({ "api": [
     "url": "/feedbacks/:id",
     "title": "Récupère un feedback identifié par son Id",
     "name": "GetFeedbackById",
-    "group": "Feedback",
+    "group": "Feedbacks",
     "description": "<p>Renvoie le feedback demandé par Id</p>",
     "parameter": {
       "fields": {
@@ -366,7 +366,7 @@ define({ "api": [
     },
     "version": "0.0.0",
     "filename": "src/api/feedbacks/index.js",
-    "groupTitle": "Feedback",
+    "groupTitle": "Feedbacks",
     "sampleRequest": [
       {
         "url": "http://localhost:4000/api/feedbacks/:id"
@@ -417,6 +417,103 @@ define({ "api": [
     ]
   },
   {
+    "type": "delete",
+    "url": "/papers/:id",
+    "title": "Supprime une copie identifiée par Id",
+    "name": "DeletePaperById",
+    "group": "Papers",
+    "description": "<p>Route permettant de supprimer une copie identifiée par son ID</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie à supprimer</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "id: 5aa00cbddfc165256122dccc",
+          "type": "String"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n    \"message\": \"copie supprimée\"\n  }",
+          "type": "html"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Papers",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/papers/:id",
+    "title": "Récupèrer une copiepar Id",
+    "name": "GetPaperById",
+    "group": "Papers",
+    "description": "<p>Une requête qui renvoit un objet au format JSON correspondant à la copie demandée</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie à afficher</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "id: 5aa00cbddfc165256122dccc",
+          "type": "String"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Papers",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/papers",
+    "title": "Récupèrer la liste de toutes les copies",
+    "name": "GetPapers",
+    "group": "Papers",
+    "description": "<p>Cette requête renvoit un objet au format JSON contenant toutes les copies existantes</p>",
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Papers",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers"
+      }
+    ]
+  },
+  {
     "type": "get",
     "url": "/evaluations/:id/papers",
     "title": "Récupère l'ensemble des copies associées à une évaluation identifiée par son Id",
@@ -462,12 +559,12 @@ define({ "api": [
     ]
   },
   {
-    "type": "delete",
-    "url": "/evaluations/:id/questions/:qid",
-    "title": "Supprime une question identifiée par id",
-    "name": "DeleteQuestion",
-    "group": "Questions",
-    "description": "<p>Une requête qui crée une question dans une évaluation donnée</p>",
+    "type": "post",
+    "url": "/papers",
+    "title": "Créer une nouvelle copie",
+    "name": "PostPaper",
+    "group": "Papers",
+    "description": "<p>Crée une nouvelle évaluation dans la base de donnée.</p> <p>La réponse est l'objet créée.</p>",
     "parameter": {
       "fields": {
         "Parameter": [
@@ -476,7 +573,100 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "id",
-            "description": "<p>ID de l'évaluation pour laquelle il faut créer la question</p>"
+            "description": "<p>ID de la copie à afficher</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "evaluationId",
+            "description": "<p>ID de l'évaluation associée à la copie</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "author",
+            "description": "<p>username de l'auteur de la copie</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "[Responses]",
+            "optional": true,
+            "field": "response",
+            "description": "<p>tableau de réponses de l'évaluation. Si absent, sera initialisé vide. Une question dont le format ne sera pas précisé aura par défaut un format &quot;textarea&quot;.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Papers",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers"
+      }
+    ]
+  },
+  {
+    "type": "put",
+    "url": "/papers/:id",
+    "title": "Modifier une évaluation",
+    "name": "UpdatePaper",
+    "group": "Papers",
+    "description": "<p>Modifie une copie</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie à afficher</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "responses",
+            "description": "<p>tableau de réponses</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Boolean",
+            "optional": false,
+            "field": "corrected",
+            "description": ""
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Papers",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id"
+      }
+    ]
+  },
+  {
+    "type": "delete",
+    "url": "/evaluations/:id/questions/:qid",
+    "title": "Supprime une question identifiée par id",
+    "name": "DeleteQuestion",
+    "group": "Questions",
+    "description": "<p>Une requête qui supprime une question dans une évaluation donnée</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de l'évaluation pour laquelle il faut supprimer la question</p>"
           },
           {
             "group": "Parameter",
@@ -673,6 +863,266 @@ define({ "api": [
     "sampleRequest": [
       {
         "url": "http://localhost:4000/api/evaluations/:id/questions"
+      }
+    ]
+  },
+  {
+    "type": "delete",
+    "url": "/papers/:id/responses/:rid",
+    "title": "Supprime une réponse identifiée par id",
+    "name": "DeleteResponse",
+    "group": "Responses",
+    "description": "<p>Une requête qui supprime une réponse dans une copie donnée</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie pour laquelle il faut supprimer la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "qid",
+            "description": "<p>ID de la réponse à supprimer</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "id: 5aa00cbddfc165256122dccc,\nqid: 5aa00cbddfc165256122dccc",
+          "type": "string"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{ message : \"la réponse a été supprimé }",
+          "type": "html"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Responses",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id/responses/:rid"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/papers/:id/responses/:rid",
+    "title": "Récupère une réponse identifiée par son Id",
+    "name": "GetResponseById",
+    "group": "Responses",
+    "description": "<p>Une requête qui renvoit une réponse donnée identifiée par son Id pour une copie donnée identifiée par son Id</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie dont il faut renvoyer la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "rid",
+            "description": "<p>ID de la réponse</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Responses",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id/responses/:rid"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/papers/:id/responses",
+    "title": "Récupère l'ensemble des réponses d'une copie identifiée par son Id",
+    "name": "GetResponsesByPaperId",
+    "group": "Responses",
+    "description": "<p>Une requête qui renvoit un tableau de réponses pour une copie donnée identifiée par son Id</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie dont il faut renvoyer les réponses</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "id: 5aa00cbddfc165256122dccc",
+          "type": "String"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Responses",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id/responses"
+      }
+    ]
+  },
+  {
+    "type": "get",
+    "url": "/evaluations/:id/questions/:qid/responses",
+    "title": "Récupère les réponses associées à une question identifiée par son Id",
+    "name": "GetResponsesByQuestionId",
+    "group": "Responses",
+    "description": "<p>Une requête qui renvoit une liste de réponses pour une question donnée identifiée par son Id pour une évaluation donnée identifiée par son Id</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de l'évaluation dont il faut renvoyer la question</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "qid",
+            "description": "<p>ID de la question dont il faut renvoyer les réponses</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "id: 5aa00cbddfc165256122dccc\nqid: 5aa00cbddfc165256122dccc",
+          "type": "String"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "src/api/evaluations/index.js",
+    "groupTitle": "Responses",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/evaluations/:id/questions/:qid/responses"
+      }
+    ]
+  },
+  {
+    "type": "post",
+    "url": "/papers/:id/responses",
+    "title": "Crée une réponse dans la copie donnée",
+    "name": "PostResponse",
+    "group": "Responses",
+    "description": "<p>Une requête qui crée une réponse dans une copie donnée</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie pour laquelle il faut créer la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "content",
+            "description": "<p>Intitulé de la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "questionId",
+            "description": "<p>id de la question associée</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Responses",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id/responses"
+      }
+    ]
+  },
+  {
+    "type": "put",
+    "url": "/papers/:id/responses/:rid",
+    "title": "Modifie une réponse identifiée par son id",
+    "name": "UpdateResponseById",
+    "group": "Responses",
+    "description": "<p>Cette route permet de modifier le contenu d'une réponse et/ou l'id du feedback associé</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>ID de la copie dont il faut modifier la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "rid",
+            "description": "<p>ID de la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "content",
+            "description": "<p>contenu de la réponse</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "feedbackId",
+            "description": "<p>Id du feedback associé à cette réponse</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/api/papers/index.js",
+    "groupTitle": "Responses",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:4000/api/papers/:id/responses/:rid"
       }
     ]
   },
--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-03-26T08:07:54.105Z",
+    "time": "2018-03-27T06:36:27.176Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
