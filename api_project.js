cff38ac8d986499955e5d7b99cc76cace5c2190c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-27 20:49:54 +0000

    Deploy EBM-2017-2018/MarkUs to github.com/EBM-2017-2018/MarkUs.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-03-27T20:32:55.402Z",
+    "time": "2018-03-27T20:49:25.283Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
